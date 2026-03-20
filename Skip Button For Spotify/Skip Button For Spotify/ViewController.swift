e7a323696954b0a1d5dbb15dccb40ccd1aeefbbf
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-20 11:53:42 -0500

    Added a timer to wait for Spotify to finish quitting before restart (Will update- very buggy)

--- Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
+++ Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var button1: NSButton!
     
+    var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,27 +27,30 @@ class ViewController: NSViewController {
 
     ///When button1 is clicked, this function will open Spotify.
     @IBAction func handleButtonClick(_ sender: Any) {
-        
+        //TODO: Use observer instead?
         if let app = NSRunningApplication.runningApplications(withBundleIdentifier: "com.spotify.client").first { //Get the first item in the collection of running apps with the bundle identifier of Spotify
             print(app)
-            print("Terminating...")
             print(app.forceTerminate())
         }
+        waitForQuit()
+    }
+    
+    func waitForQuit() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startSpotifyOnTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func startSpotifyOnTimer() {
         // Source - https://stackoverflow.com/a/58241536
         // Posted by vookimedlo
         // Retrieved 2026-03-19, License - CC BY-SA 4.0
-
         guard let url: URL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.spotify.client") else {
             return } //Get the URL for the application with the identifier .com.apple.Terminal
-        print("Restarting...")
 
         let configuration = NSWorkspace.OpenConfiguration()
         configuration.arguments = ["/bin"]
         NSWorkspace.shared.openApplication(at: url,
                                            configuration: configuration,
                                            completionHandler: nil)
-        
-
     }
     
     
