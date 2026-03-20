b001792151fbdbec60e576b5bc79dfe19eb524c4
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-19 23:42:18 -0500

    Added app restart functionality

--- Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
+++ Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
@@ -25,12 +25,18 @@ class ViewController: NSViewController {
 
     ///When button1 is clicked, this function will open Spotify.
     @IBAction func handleButtonClick(_ sender: Any) {
-        print("Click!")
+        
+        if let app = NSRunningApplication.runningApplications(withBundleIdentifier: "com.spotify.client").first { //Get the first item in the collection of running apps with the bundle identifier of Spotify
+            print("Terminating...")
+            app.terminate()
+        }
         // Source - https://stackoverflow.com/a/58241536
         // Posted by vookimedlo
         // Retrieved 2026-03-19, License - CC BY-SA 4.0
 
-        guard let url: URL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.spotify.client") else { return } //Get the URL for the application with the identifier .com.apple.Terminal
+        guard let url: URL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.spotify.client") else {
+            return } //Get the URL for the application with the identifier .com.apple.Terminal
+        print("Restarting...")
 
         let configuration = NSWorkspace.OpenConfiguration()
         configuration.arguments = ["/bin"]
