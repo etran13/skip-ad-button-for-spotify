d279c0d91e7cfedae0eea1daf97cf07826ffa891
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-19 23:08:07 -0500

    Changed project idea to MacOS app instead of Python script

---
+++ Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Skip Button For Spotify
+//
+//  Created by Ellie Tran on 3/19/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
