4b07f73ebb24ce5e67e1cf4500c312f6d2a6ee89
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-22 13:31:57 -0500

    Changed the button text from "Restart Spotify" to "Skip Ad", deleted main.py

--- Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
+++ Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
@@ -32,6 +32,7 @@ class ViewController: NSViewController {
         guard let app = NSRunningApplication.runningApplications(withBundleIdentifier: "com.spotify.client").first else {
             return
         }
+        print("App Instance: \(app)")
             
         let starter = SpotifyStarter(object: app) //Create an observer of app in order to recieve notification on termination
         
