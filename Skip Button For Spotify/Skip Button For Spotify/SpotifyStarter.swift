61804e34244a38ac3451a501c53d0e8266546f1f
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-22 12:52:31 -0500

    Finished changing the restart function to rely on Observer instead of a timer to ensure a faster restart

--- Skip Button For Spotify/Skip Button For Spotify/SpotifyStarter.swift
+++ Skip Button For Spotify/Skip Button For Spotify/SpotifyStarter.swift
@@ -21,8 +21,29 @@ class SpotifyStarter: NSObject {
         observation = observe(
             \.objectToObserve.isTerminated,
         ) { object, change in
-            print("isTerminated just changed to True")
+            self.startSpotify()
         }
         
+    }
+    
+    func startSpotify() {
+        print("isTerminated just changed to True")
+        // Source - https://stackoverflow.com/a/58241536
+        // Posted by vookimedlo
+        // Retrieved 2026-03-19, License - CC BY-SA 4.0
+        guard let url: URL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.spotify.client") else {
+            return } //Get the URL for the application with the identifier .com.apple.Terminal
+
+        let configuration = NSWorkspace.OpenConfiguration()
+        configuration.arguments = ["/bin"]
+        NSWorkspace.shared.openApplication(at: url,
+                                           configuration: configuration,
+                                           completionHandler: nil)
+        //self.stopWatching()
+    }
+    
+    func stopWatching() {
+        // Setting to nil cancels the observation automatically
+        observation = nil
     }
 }
--- Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
+++ Skip Button For Spotify/Skip Button For Spotify/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: NSViewController {
     
     var timer: Timer?
     
-    var spotifyController: SpotifyController?
+    var spotifyStarter: SpotifyStarter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,34 +29,14 @@ class ViewController: NSViewController {
 
     ///When button1 is clicked, this function will quit Spotify.
     @IBAction func handleButtonClick(_ sender: Any) {
-        //TODO: Use observer instead?
-        if let app: NSRunningApplication = NSRunningApplication.runningApplications(withBundleIdentifier: "com.spotify.client").first { //Get the first item in the collection of running apps with the bundle identifier of Spotify
-            
-            
-            
-            print(app)
-            print(app.forceTerminate())
-            print("Terminated: " + String(app.isTerminated))
+        guard let app = NSRunningApplication.runningApplications(withBundleIdentifier: "com.spotify.client").first else {
+            return
         }
+            
+        let starter = SpotifyStarter(object: app) //Create an observer of app in order to recieve notification on termination
+        
+        app.forceTerminate()
         //waitForQuit()
-    }
-    
-    func waitForQuit() {
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startSpotifyOnTimer), userInfo: nil, repeats: true)
-    }
-    
-    @objc func startSpotifyOnTimer() {
-        // Source - https://stackoverflow.com/a/58241536
-        // Posted by vookimedlo
-        // Retrieved 2026-03-19, License - CC BY-SA 4.0
-        guard let url: URL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: "com.spotify.client") else {
-            return } //Get the URL for the application with the identifier .com.apple.Terminal
-
-        let configuration = NSWorkspace.OpenConfiguration()
-        configuration.arguments = ["/bin"]
-        NSWorkspace.shared.openApplication(at: url,
-                                           configuration: configuration,
-                                           completionHandler: nil)
     }
     
     
