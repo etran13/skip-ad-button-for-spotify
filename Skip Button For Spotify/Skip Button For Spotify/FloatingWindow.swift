48910aa6f658d156007d64a8ee0f50ea1e3b4efc
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-23 22:40:18 -0500

    Added new class for floating window

---
+++ Skip Button For Spotify/Skip Button For Spotify/FloatingWindow.swift
@@ -0,0 +1,16 @@
+//
+//  FloatingWindow.swift
+//  Skip Button For Spotify
+//
+//  Created by Ellie Tran on 3/23/26.
+//
+
+import Cocoa
+
+class FloatingWindow: NSWindow {
+    
+    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+        super.init()
+    }
+
+}
