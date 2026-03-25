b5a48f8b170fa9e80526518347038b8de2076475
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-25 10:12:42 -0500

    Updated window appearance to be translucent in order to make it less intrusive

--- Skip Button For Spotify/Skip Button For Spotify/FloatingWindow.swift
+++ Skip Button For Spotify/Skip Button For Spotify/FloatingWindow.swift
@@ -10,7 +10,9 @@ import Cocoa
 class FloatingWindow: NSWindow {
     
     override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
-        super.init()
+        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+        print("NSWINDOW SUBCLASS ACTIVE")
+        self.backgroundColor = NSColor(calibratedHue: 0.5, saturation: 1.0, brightness: 0, alpha: 0.6)
     }
 
 }
