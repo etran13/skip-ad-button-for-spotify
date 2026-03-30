a0cc169a3a4523921fd9507b0a84b588e8ac5c99
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-03-30 13:00:58 -0500

    Switched out button image for a borderless PNG and re-added toolbar to top of window

--- Skip Button For Spotify/Skip Button For Spotify/FloatingWindow.swift
+++ Skip Button For Spotify/Skip Button For Spotify/FloatingWindow.swift
@@ -10,11 +10,11 @@ import Cocoa
 class FloatingWindow: NSWindow {
     
     override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
-        let style: NSWindow.StyleMask = .borderless //Set it up to appear without a toolbar
-        let frame = NSRect(x: 0, y: 0, width: 256, height: 256)
+        let frame = NSRect(x: 0, y: 0, width: 501, height: 105)
         let contentRect = NSWindow.contentRect(forFrameRect: frame, styleMask: .borderless)
         
         super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag) //Call superclass init
+        print(contentRect)
         
         self.backgroundColor = NSColor(calibratedHue: 0.5, saturation: 1.0, brightness: 0, alpha: 0.6) //Set the transparency and background color
         
